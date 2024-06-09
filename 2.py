32ec517f6af40891a8e6c0af42c4fdcd94892906
Ivanskem <150799423+Ivanskem@users.noreply.github.com>
2024-06-09 12:30:09 +0700

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,7 @@
 import discord
-from discord.ext import commands
-import asyncio
-import flet as ft
+# from discord.ext import commands
+# import asyncio
+# import flet as ft
 
 # Создайте объект Intents и установите необходимые флаги
 intents = discord.Intents.default()
@@ -42,6 +42,9 @@ async def on_message(message):
     if message.content.startswith('.members') and any(role.name == "Администратор" for role in message.author.roles):
         await handle_members(message)
 
+    if message.content.startswith('.member') and any(role.name == "Администратор" for role in message.author.roles):
+        await handle_memberinfo(message)
+
     if message.content.startswith('.help') and any(role.name == "Администратор" for role in message.author.roles):
         await handle_help(message)
 
@@ -53,6 +56,9 @@ async def on_message(message):
 
     if message.content.startswith('.unban') and any(role.name == 'Администратор' for role in message.author.roles):
         await handle_unban(message)
+
+    if message.content.startswith('.avatar') and any(role.name == 'Администратор' for role in message.author.roles):
+        await handle_avatar(message)
 # Функция для обработки команды .ban
 async def handle_ban(message):
     async for msg in message.channel.history(limit=1):
@@ -139,8 +145,12 @@ async def handle_help(message):
     async for msg in message.channel.history(limit=1):
         await msg.delete()
     embed = discord.Embed(title="Доступные команды сервера", color=0xffffff)
-    embed.add_field(name="Модерация", value=f"Блокировка: .ban @Нарушитель причина \nРазблокировка: .unban @Нарушитель причина \nУдаление: .kick @Нарушитель причина \nОтчистка: .clear количество(можно цифрой либо all для удаления всего \nСписок всех учатников: .members \nВывод информации о сервере: .serverinfo(писать только в канал статистика) \nЗаглушение участника: .mute @Нарушитель причина \nРазглушение участника: .unmute @Нарушитель причина", inline=False)
+    embed.add_field(
+    name="Модерация", value=f"Блокировка: .ban @Нарушитель причина \nРазблокировка: .unban @Нарушитель причина \nУдаление: .kick @Нарушитель причина \nОтчистка: .clear количество(можно цифрой либо all для удаления всего \nСписок всех учатников: .members \nВывод информации о сервере: .serverinfo(писать только в канал статистика) \nЗаглушение участника: .mute @Нарушитель причина"f" \nРазглушение участника: .unmute @Нарушитель причина \nИнформация о участнике: .member @Участник \nАватар участника: .avatar @Участник", inline=False
+    )
     chanel_mod = discord.utils.get(message.guild.channels, name="bot-commands")
+    async for msg in chanel_mod.history(limit=1):
+        await msg.delete()
     await chanel_mod.send(embed=embed)
 async def handle_mute(message):
     async for msg in message.channel.history(limit=1):
@@ -198,4 +208,50 @@ async def handle_unban(message):
             await message.channel.send(f'Пользователь с ID {user_id} не найден.')
     else:
         await message.channel.send('Неверный формат команды. Используйте: .unban <@пользователь> причина')
+
+async def handle_memberinfo(message):
+    async for msg in message.channel.history(limit=1):
+        await msg.delete()
+    parts = message.content.split(' ')
+    if len(parts) > 1:
+        # Ищем пользователя по упоминанию
+        member = message.mentions[0]
+    else:
+        # Если пользователь не указан, используем автора сообщения
+        member = message.author
+    # if isinstance(message.author, discord.Member):
+    #     member = message.author
+    # else:
+    #     await message.channel.send(
+    #         "Извини, я не могу получить информацию о пользователе, который не является членом сервера.")
+    #     return
+    # role_list = [role.name for role in member.roles]
+    # role_string = ", ".join(role_list)
+    # discriminator = member.discriminator
+    # if discriminator == 0:
+    #     discriminator = None
+    embed = discord.Embed(title=f"Информация о {member.name}", color=0xffffff)
+    embed.set_thumbnail(url=member.avatar.url)
+    embed.add_field(name="Никнейм:", value=member.name, inline=True)
+    embed.add_field(name="Профиль:", value=member.mention, inline=True)
+    # embed.add_field(name="Полное имя:", value=discriminator, inline=True)
+    embed.add_field(name="ID:", value=member.id, inline=True)
+    embed.add_field(name="Дата присоеденения:", value=member.joined_at.strftime("%Y-%m-%d %H-%M"), inline=True)
+    embed.add_field(name="Роли:", value=member.top_role.name, inline=True)
+    # embed.add_field(name="Статус:", value=member.status, inline=True)
+
+    await message.channel.send(embed=embed)
+async def handle_avatar(message):
+    async for msg in message.channel.history(limit=1):
+        await msg.delete()
+    parts = message.content.split()
+
+    if len(parts) > 1:
+        member = message.mentions[0]
+    else:
+        member = message.author
+
+    embed = discord.Embed(title=f"Аватар {member.name}", color=0xffffff)
+    embed.set_image(url=member.avatar.url)
+    await message.channel.send(embed=embed)
 client.run('MTI0NjY1Mzg3MjQyMDY4Mzg4Ng.G1N5gg.2vL4aj8ZWornQxSTKwNgjG9aBvQ6CNL_az9tOg')
